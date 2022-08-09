30b742cad1e0704d261937882f7de12e54f5e985
maxt <Joycechu@Joycechude-MacBook-Pro.local>
2022-08-10 00:37:59 +0800

    Update romanToInt     easy         done&pass

--- romanToInt.c
+++ romanToInt.c
@@ -20,49 +20,27 @@ int romanCharToInt(char c)
 {
     switch (c)
     {
-    case 'I':
-        return 1;
-    case 'V':
-        return 5;
-    case 'X':
-        return 10;
-    case 'L':
-        return 50;
-    case 'C':
-        return 100;
-    case 'D':
-        return 500;
-    case 'M':
-        return 1000;
-    default:
-        return 0;
+    case 'I': return 1;
+    case 'V': return 5;
+    case 'X': return 10;
+    case 'L': return 50;
+    case 'C': return 100;
+    case 'D': return 500;
+    case 'M': return 1000;
+    default : return 0;
     }
 }
 int main(){
-    int i = 0;
-    int value = 0;
-    int temp = 0;
-    char* s="LVIII";
-    //for(int i=0;i<sizeof(s)-1;i++){
+    int i       = 0;
+    int value   = 0;
+    char* s     ="LVIII";
     while( i < sizeof(s)-1 ){
-        temp=(romanCharToInt(s[i+1]) - romanCharToInt(s[i]));
-        printf("\n-----i=%d temp=%d Ivalue=%d\t", i, temp, romanCharToInt(s[i]));
-        switch(temp){
-            case 4:
-            case 40:
-            case 400:
-            case 9:
-            case 90:
-            case 900:
-                value+=temp;
-                printf("\n value=%d", value);
-                i+=2;
-                break;
-            default :
-                value+=romanCharToInt(s[i]);
-                printf("\n value=%d", value);
-                i++;
-                break;
+        if(romanCharToInt(s[i+1]) > romanCharToInt(s[i])){
+            value+=(romanCharToInt(s[i+1]) + romanCharToInt(s[i]));
+            i+=2;
+        }else{
+            value+=romanCharToInt(s[i]);
+            i++;
         }
     }
     printf("\n value=%d", value);
