2d98473a1ef8e510c123ebe35b9f544edc7eb851
maxt <Joycechu@Joycechude-MacBook-Pro.local>
2022-08-16 23:07:21 +0800

    Add lengthOfLongestSubstring med ongoing

---
+++ lengthOfLongestSubstring.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int lengthOfLongestSubstring(char * s){
+    int len=0;
+
+    return len;
+}
+
+int main(){
+    int len;
+    char* s = "pwwkew"; // output: 3 -> wke
+    len = lengthOfLongestSubstring(s);
+    printf("%d", len);
+    return len;
+}
